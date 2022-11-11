49cff51eacb9d0c49e6b5fac4cf6c0caa82b3021
yurididomenico <yurididomenico93@gmail.com>
2022-11-11 14:54:09 +0100

    JS: Collegamento immagine da js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,11 +4,25 @@ console.log("YDD");
 
 /*
 Slider:
-Inserire 2 frecce a sinistra e a destra dell'immagine momentanea dello slider che permettono di andare avanti e indietro tra le immagini
+-Inserire 2 frecce a sinistra e a destra dell'immagine momentanea dello slider
+ che permettono di andare avanti e indietro tra le immagini
 */
 
 
+var app = new Vue
+(
+    {
+        el: '#root',
+        data: 
+        {
+            immagini: "https://picsum.photos/800/400"
+        },
+        methods:
+        {
 
+        }
+    }
+);
 
 
 
