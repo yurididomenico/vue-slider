1fb0c6624eff0d86dbfef5e854f326a2b189bb4a
yurididomenico <yurididomenico93@gmail.com>
2022-11-11 14:42:52 +0100

    HTML: Struttura Slider

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,10 @@
 
 console.log("YDD");
 
-
+/*
+Slider:
+Inserire 2 frecce a sinistra e a destra dell'immagine momentanea dello slider che permettono di andare avanti e indietro tra le immagini
+*/
 
 
 
